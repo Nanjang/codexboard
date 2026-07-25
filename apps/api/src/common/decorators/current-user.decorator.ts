d463fa2870819a3f71a624534a79de6b11199f90
octopus7 <octopus7net@gmail.com>
2026-07-25 16:11:49 +0900

    새로시작

--- apps/api/src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { SessionUser } from '../types/session';
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): SessionUser | undefined => {
-    const req = ctx.switchToHttp().getRequest<{ session?: { user?: SessionUser } }>();
-    return req.session?.user;
-  }
-);
--- apps/api/src/modules/bbs/bbs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BbsController } from './bbs.controller';
-import { BbsService } from './bbs.service';
-
-@Module({
-  controllers: [BbsController],
-  providers: [BbsService]
-})
-export class BbsModule {}
